2ea2f61d59cf58172a9301d1793dc158b6280531
Ben Hatsor <34835685+benhatsor@users.noreply.github.com>
2023-12-23 18:02:18 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
 
-
+const introScreen = document.querySelector('.intro.screen');
+const uploadButtons = introScreen.querySelectorAll('.upload.button');
+
+uploadButtons.forEach(button => {
+  
+  button.addEventListener('click', () => {
+    
+    button.classList.toggle('selected');
+    
+  });
+  
+});
+
