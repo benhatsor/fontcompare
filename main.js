54b48020ce81b64e3c80a07bb08dd9ca1577370e
Ben Hatsor <34835685+benhatsor@users.noreply.github.com>
2023-12-23 18:11:26 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,13 +2,54 @@
 const introScreen = document.querySelector('.intro.screen');
 const uploadButtons = introScreen.querySelectorAll('.upload.button');
 
+let font1, font2;
+
 uploadButtons.forEach(button => {
   
-  button.addEventListener('click', () => {
+  button.addEventListener('click', async () => {
     
-    button.classList.toggle('selected');
+    const font = await uploadFont();
+    
+    if (button.classList.contains('font1')) {
+      
+      font1 = font;
+      
+    } else if (button.classList.contains('font2')) {
+      
+      font2 = font;
+      
+    }
+    
+    button.classList.add('selected');
     
   });
   
 });
+
+
+// utils
+
+function uploadFont() {
+  
+  let input = document.createElement('input');
+  input.type = 'file';
+  input.accept = 'font/*';
+  
+  return new Promise(resolve => {
+    
+    input.addEventListener('change', () => {
+      
+      if (this.files && this.files[0]) {
+        
+        const file = this.files[0];
+        
+        resolve(file);
+        
+      }
+      
+    });
+    
+  });
+  
+}
 
